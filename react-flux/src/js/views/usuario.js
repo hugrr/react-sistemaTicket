21169e723b2427fe7ada0afb57b8408da0bbbb1d
Francisco Badilla <f.badilla228@gmail.com>
2019-07-24 21:52:44 +0000

    usuarios mod

--- react-flux/src/js/views/usuario.js
+++ react-flux/src/js/views/usuario.js
@@ -4,49 +4,83 @@ export class Usuarios extends Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<form>
-					<div className="form-row">
-						<div className="col-md-6 mb-6">
-							<div className="col-md-4 mb-3">
-								<label htmlFor="validationDefault01">Nombre</label>
-								<input
-									type="text"
-									className="form-control"
-									id="validationDefault01"
-									placeholder="First name"
-									value="Francisco"
-									required
-								/>
-							</div>
-							<div className="col-md-4 mb-3">
-								<label htmlFor="validationDefault02">Apellido</label>
-								<input
-									type="text"
-									className="form-control"
-									id="validationDefault02"
-									placeholder="Badilla"
-									value="Otto"
-									required
-								/>
-							</div>
-							<div className="col-md-4 mb-3">
-								<label htmlFor="validationDefaultUsername">Usuario</label>
-								<div className="input-group">
-									<div className="input-group-prepend">
-										<span className="input-group-text" id="inputGroupPrepend2">
-											@
-										</span>
-									</div>
+				<h1>usuario</h1>
+				<div className="container">
+					<div className="row justify-content-md-center">
+						<div className="text-center col-md-4 mb-4">
+							<form>
+								<div className="form-row justify-content-md-center">
+									<label htmlFor="validationDefault01">Nombre</label>
 									<input
 										type="text"
 										className="form-control"
-										id="validationDefaultUsername"
-										placeholder="Username"
-										aria-describedby="inputGroupPrepend2"
+										id="validationDefault01"
+										placeholder="First name"
+										value="Francisco"
 										required
 									/>
+									<label htmlFor="validationDefault02">Email</label>
+									<input
+										type="email"
+										className="form-control"
+										id="validationDefault02"
+										placeholder="Email"
+										value="usuario@usuario.com"
+										required
+									/>
+									<label htmlFor="validationDefault02">telefono</label>
+									<input
+										type="text"
+										className="form-control"
+										id="validationDefault02"
+										placeholder="+569 6666 6666"
+									/>
+									<label htmlFor="validationDefault02">Fecha de Nacimiento</label>
+									<input
+										type="text"
+										className="form-control"
+										id="validationDefault02"
+										placeholder="+569 6666 6666"
+									/>
+									<label htmlFor="validationDefault02">Contraseña</label>
+									<input
+										type="password"
+										className="form-control"
+										id="inputPassword"
+										placeholder="Contraseña"
+									/>
+									<button type="button" className="btn btn-primary btn-sm" disabled>
+										modificar
+									</button>
+									<button type="submit" className="btn btn-secondary btn-sm">
+										crear
+									</button>
 								</div>
+							</form>
+						</div>
+						<div className="text-center col-md-4 mb-4">
+							<label htmlFor="validationDefaultUsername">grupo</label>
+							<div className="input-group">
+								<div className="input-group-prepend">
+									<span className="input-group-text" id="inputGroupPrepend2">
+										@
+									</span>
+								</div>
+								<input
+									type="text"
+									className="form-control"
+									id="validationDefaultUsername"
+									placeholder="nombre del grupo"
+									aria-describedby="inputGroupPrepend2"
+									required
+								/>
+								<button className="btn btn-primary" type="text">
+									Buscar
+								</button>
 							</div>
+							<label htmlFor="validationDefault02">
+								<h2>Terminos y condiciones</h2>
+							</label>
 							<div className="form-group">
 								<div className="form-check">
 									<input
@@ -57,7 +91,7 @@ export class Usuarios extends Component {
 										required
 									/>
 									<label className="form-check-label" htmlFor="invalidCheck2">
-										acepto los terminos 1
+										map. terminos y condiciones
 									</label>
 								</div>
 							</div>
@@ -89,12 +123,12 @@ export class Usuarios extends Component {
 									</label>
 								</div>
 							</div>
+							<button className="btn btn-primary" type="submit">
+								Ingresar
+							</button>
 						</div>
-						<button className="btn btn-primary" type="submit">
-							cambiar
-						</button>
 					</div>
-				</form>
+				</div>
 			</div>
 		);
 	}
