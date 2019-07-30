303f6ce9be993df6639162c2f988bdf717457398
Hugo Rojas <hurojas.rios@gmail.com>
2019-07-30 20:42:14 +0000

    anunci actualizado falto mostra html

--- react-flux/src/js/views/notice.js
+++ react-flux/src/js/views/notice.js
@@ -16,11 +16,7 @@ export class Notice extends React.Component {
 					<p className="card-text">
 						Some quick example text to build on the card title and make up the bulk of the content.
 					</p>
-					<button
-						type="button"
-						className="btn btn-primary"
-						data-toggle="modal"
-						data-target="#exampleModalLong">
+					<button type="button" className="btn btn-dark" data-toggle="modal" data-target="#exampleModalLong">
 						Crear Aviso
 					</button>
 					<Modal />
