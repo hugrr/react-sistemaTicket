f75b7ad34e597b1681cc7507a398d11fc03ea56e
Hugo Rojas <hurojas.rios@gmail.com>
2019-07-31 19:01:49 +0000

    anuncio conectado con api

--- react-flux/src/js/store/flux.js
+++ react-flux/src/js/store/flux.js
@@ -22,9 +22,7 @@ const getState = ({ getStore, setStore }) => {
 
 				fetch("https://3000-d3fb5b3d-a077-40c3-8690-5eb8d6548d50.ws-us0.gitpod.io/api/anuncio/", {
 					method: "Post",
-					body: JSON.stringify({
-						data
-					}),
+					body: JSON.stringify(data),
 					headers: {
 						"Content-Type": "application/json"
 					}
