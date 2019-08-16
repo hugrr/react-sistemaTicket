40c5bd4873397f8656bbb7ea3c1867273142da31
Hugo Rojas <hurojas.rios@gmail.com>
2019-08-16 17:27:17 +0000

    guradando votacion y aviso bienvenida. falta crear funciones

--- react-flux/src/js/component/cardNewmember.js
+++ react-flux/src/js/component/cardNewmember.js
@@ -6,14 +6,18 @@ export default class Anuncio extends React.Component {
 	render() {
 		return (
 			<div className="container">
+                <div className="row">
+            <div calssName="col 3-md">
 				<div className="card text-white bg-dark mb-3">
-					<div className="card-header">hola</div>
+					<div className="card-header">aviso</div>
 					<div className="card-body">
-						<h5 className="card-title">buen diaa!</h5>
-						<p className="card-text">Te damos la bienvenida a LIke a Club</p>
+						<h5 className="card-title">avisoNAme</h5>
+						<p className="card-text">descripton</p>
 					</div>
 				</div>
 			</div>
+            </div>
+            </div>
 		);
 	}
 }
