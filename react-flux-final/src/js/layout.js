15244ee10ca692655e9d7934e765b2767bb4d668
fbadilla <36639028+fbadilla@users.noreply.github.com>
2019-08-20 15:25:07 -0400

    base nueva react

---
+++ react-flux-final/src/js/layout.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+
+import { Home } from "./views/home";
+import injectContext from "./store/appContext";
+
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+
+//create your first component
+export class Layout extends React.Component {
+	render() {
+		//the basename is used when your project is published in a subdirectory and not in the root of the domain
+		// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+		const basename = process.env.BASENAME || "";
+
+		return (
+			<div className="page-inner">
+				<BrowserRouter basename={basename}>
+					<Fragment>
+						<Navbar />
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route render={() => <h1>Not found!</h1>} />
+						</Switch>
+						<Footer />
+					</Fragment>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default injectContext(Layout);
---
+++ react-flux-final/src/js/store/flux.js
@@ -0,0 +1,106 @@
+const getState = ({ getStore, getActions, setStore }) => {
+	return {
+		store: {
+			apiUrl: "",
+			token: {
+				refresh: "",
+				access: ""
+			},
+			avisos: [],
+			grupos: [],
+			miembros: [],
+			miembro: {},
+			username: "",
+			password: "",
+			password2: "",
+			error: {}
+		},
+		actions: {
+			handleChange: e => {
+				const { name, value } = e.target;
+				setStore({
+					[name]: value
+				});
+			},
+			handleLogin: e => {
+				e.preventDefault();
+				const store = getStore();
+				getActions().login(store.username, store.password);
+			},
+			handleRegister: e => {
+				e.preventDefault();
+			},
+			login: (username, password) => {
+				const store = getStore();
+				const data = {
+					username: username,
+					password: password
+				};
+				fetch(store.apiUrl + "/api/token", {
+					method: "GET",
+					body: JSON.stringify(data),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => setStore({ data }));
+			},
+			register: (username, password) => {
+				console.log(username);
+				console.log(password);
+			},
+			getAvisos: () => {
+				const store = getStore();
+				fetch(store.apiUrl + "/api/avisos", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer " + store.token.access
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => setStore({ avisos: data }))
+					.catch(error => setStore({ error }));
+			},
+			getMiembros: () => {
+				const store = getStore();
+				fetch(store.apiUrl + "/api/miembros", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer " + store.token.access
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => setStore({ miembros: data }));
+			},
+			getMiembro: () => {
+				const store = getStore();
+				fetch(store.apiUrl + "/api/miembro", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer " + store.token.access
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => setStore({ miembro: data }));
+			},
+			getGrupos: () => {
+				const store = getStore();
+				fetch(store.apiUrl + "/api/grupos", {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: "Bearer " + store.token.access
+					}
+				})
+					.then(resp => resp.json())
+					.then(data => setStore({ grupos: data }));
+			}
+		}
+	};
+};
+
+export default getState;
